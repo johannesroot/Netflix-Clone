eda316f3a6b36191e5396745aa40bf1be8432d2c
johannesroot <yohannesgigar21@gmail.com>
2024-11-20 02:55:49 -0900

    banner

---
+++ Netflix/src/components/Banner/Banner.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import './Banner.css';
+import instance from "../../utils/Axios";
+import requests from "../../utils/Requests";
+
+const Banner = () => {
+  const [movie, setMovie] = useState({}); // State to hold a movie
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await instance.get(requests.fetchNetflixOriginals);
+        const movies = response.data.results;
+        setMovie(movies[Math.floor(Math.random() * movies.length)]);
+      } catch (error) {
+        console.error("Error:", error.response?.data || error.message);
+      }
+    };
+
+    fetchMovie(); // Call the async function inside useEffect
+  }, []);
+
+  return (
+    <>
+      <div
+        className="banner"
+        style={{
+          backgroundSize: 'cover',
+          backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path || ''}')`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className="banner_contents">
+          <h1 className="banner_title">
+            {movie?.title || movie?.name || movie?.original_name || 'No Title'}
+          </h1>
+          <div className="banner_buttons">
+            <button className="banner_button play">Play</button>
+            <button className="banner_button">My List</button>
+          </div>
+          {movie?.overview && (
+            <h1 className="banner_description">
+              {movie.overview.length > 150
+                ? movie.overview.slice(0, 147) + '...'
+                : movie.overview}
+            </h1>
+          )}
+        </div>
+        <div className="banner_fadeBottom" />
+      </div>
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export default Banner;
--- Netflix/src/utils/Requests.jsx
+++ Netflix/src/utils/Requests.jsx
@@ -1,7 +1,6 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
-
-const reqquests = {
-
+const API_KEY = import.meta.env.VITE_API_KEY;// console.log("API_KEY:", process.env.REACT_APP_API_KEY);
+ 
+const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -10,7 +9,6 @@ const reqquests = {
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
-}
-
-export default reqquests;
+    fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+};
+export default requests;
